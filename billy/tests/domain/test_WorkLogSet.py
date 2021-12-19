af98ed4cc00e96282d2c9d3bda926169d6cd2b16
Emile Tenezakis <e.tenezakis@gmail.com>
2021-12-19 18:59:08 +0900

    bug

--- billy/tests/domain/test_WorkLogSet.py
+++ billy/tests/domain/test_WorkLogSet.py
@@ -1,11 +1,11 @@
 from billy.domain.WorkSessionSimple import WorkSessionSimple
 from billy.domain.WorkLogSet import WorkLogSet
-
+import pendulum as pdl
 
 def test_add_session():
 
     mySession = WorkSessionSimple()
-    myLog = WorkLogSet()
+    myLog = WorkLogSet(client="A", project="1")
     myLog.add_session(mySession)
     assert mySession in myLog
 
@@ -13,13 +13,15 @@ def test_add_session():
 def test_has_session():
 
     mySession = WorkSessionSimple()
-    myLog = WorkLogSet(client="A", project="1", sessions=set([mySession]))
+    myLog = WorkLogSet(client="A", project="1", sessions=set(mySession))
     assert mySession in myLog
 
 
 def test_total_time():
 
+    start = pdl.now()
     myLog = WorkLogSet(client="A", project="1", sessions=set())
     myLog.add_session(WorkSessionSimple().end_session())
     myLog.add_session(WorkSessionSimple().end_session())
-    assert myLog.total_time() == 0
+    end = pdl.now()
+    assert myLog.total_time(start, end) == 0
