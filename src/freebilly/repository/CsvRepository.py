9c84e454338d87c589daef508a6919a0013cc7dd
Emile Tenezakis <e.tenezakis@gmail.com>
2021-12-23 15:23:56 +0900

    implemented CsvRepository

--- src/freebilly/repository/CsvRepository.py
+++ src/freebilly/repository/CsvRepository.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 from pathlib import Path
 import csv
 from src.freebilly.repository.AbstractRepository import AbstractRepository
@@ -14,14 +14,14 @@ class CsvRepository(AbstractRepository):
     a work session in that work log, with a start time and an end_time column.
     """
 
-    __work_log_paths: Path
+    __work_log_path: Path
     __work_log_prefix: "work_log"
     __work_log: Union[None, AbstractWorkLog]
+    __field_names = ['start_time', 'end_time']
 
     def __init__(self, path: Path) -> None:
 
         """
-
         Parameters
         ----------
         path: Path
@@ -32,19 +32,45 @@ class CsvRepository(AbstractRepository):
         self.__work_log_path = path
         self._work_log = None
 
+
     def push(self, work_log: AbstractWorkLog) -> None:
 
-        raise NotImplementedError
+        with open(self.get_csv_file_path(work_log.get_client(), work_log.get_project()), 'w', newline='') as csvfile:
+            work_log_writer = csv.DictWriter(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL, self.__field_names)
+            work_log_writer.writeheader()
+            for work_session in work_log:
+                work_log_writer.writerow({'start_time': work_session.get_start_time(), 'end_time': work_session.get_end_time()})
 
     def exists(self, client: str, project: str) -> bool:
 
-        raise NotImplementedError
+        return Path(self.get_csv_file_path(client, project)).exists()
 
     def get(self, client: str, project: str) -> AbstractWorkLog:
 
+        csv_file_path = self.get_csv_file_path(client, project)
+        if not self.exists(client, project)
+            raise ValueError(f'file for client {client} and project {project} does not exist')
         new_work_log = SetWorkLog(client, project) #TODO here is a coupling with SetWorkLog
-        with open(str(self.__work_log_path.joinpath(self.__work_log_prefix + "_" + client + "_" + project))) as csv_file:
+        with open(csv_file_path) as csv_file:
             for row in csv.DictReader(csv_file, fieldnames=['start_time', 'end_time'], delimiter=' ', quotechar='|'):
                 new_work_log.add_session(PendulumWorkSession(pdl.parse(row['start_time']), pdl.parse(row['end_time'])))
 
         return new_work_log
+
+    def get_csv_file_path(self, client: str, project: str) -> Path:
+
+        """
+
+        Parameters
+        ----------
+        client: str
+        project: str
+
+        Returns
+        -------
+        Path
+            full path to csv file containing that work log
+        """
+
+        return str(self.__work_log_path.joinpath(self.__work_log_prefix + "_" + client + "_" + project + ".csv"))
