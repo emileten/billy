7947dc7661c8817fdb05e3338801a5763cf24115
Emile Tenezakis <e.tenezakis@gmail.com>
2021-12-23 15:28:20 +0900

    reformat

--- tests/repository/test_CsvRepository.py
+++ tests/repository/test_CsvRepository.py
@@ -10,17 +10,16 @@ from src.freebilly.domain.SetWorkLog import SetWorkLog
 # TODO duplicate stuff, consider fixtures
 
 def test_exists():
-
     temp_folder_path = '/tmp'
     with NamedTemporaryFile(dir=temp_folder_path) as temp_fp:
-        os.rename(temp_fp.name, str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv'))) # because no full name choice option
+        os.rename(temp_fp.name,
+                  str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv')))  # because no full name choice option
         repo = CsvRepository(Path('/tmp'))
-        assert repo.exists('A','1')
+        assert repo.exists('A', '1')
         os.rename(str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv')), temp_fp.name)  # finally, rename back
 
 
 def test_exists_push():
-
     with TemporaryDirectory() as fake_dir_path:
         repo = CsvRepository(Path(fake_dir_path.name))
         empty_work_log = SetWorkLog(client='A', project='1')
@@ -29,19 +28,19 @@ def test_exists_push():
 
 
 def test_exists_get():
-
     temp_folder_path = '/tmp'
     with NamedTemporaryFile(dir=temp_folder_path) as temp_fp:
-        os.rename(temp_fp.name, str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv'))) # because no full name choice option
+        os.rename(temp_fp.name,
+                  str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv')))  # because no full name choice option
         repo = CsvRepository(Path('/tmp'))
-        assert repo.exists('A','1')
-        my_work_log : repo.get('A', '1')
+        assert repo.exists('A', '1')
+        my_work_log: repo.get('A', '1')
         assert isinstance(my_work_log, AbstractWorkLog)
         assert my_work_log.is_empty()
         os.rename(str(Path().joinpath(temp_folder_path, 'work_log_A_1.csv')), temp_fp.name)  # finally, rename back
 
-def test_get_fails():
 
+def test_get_fails():
     repo = CsvRepository(Path('/tmp'))
     with pytest.raises(ValueError):
         repo.get('A', '1')
